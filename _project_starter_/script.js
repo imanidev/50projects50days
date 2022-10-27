3c2991d751c085f6cadc22ad695b75d9a97bb1b4
imani.devs <imaniroberts.dev@gmail.com>
2022-10-27 19:16:33 -0400

    added js

--- _project_starter_/script.js
+++ _project_starter_/script.js
@@ -1,1 +1,14 @@
+const panels = document.querySelectorAll('.panel')
 
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        panel.classList.add('active')
+        removeActiveClasses
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(() => {
+        panel.classList.remove('active')
+    })
+}
